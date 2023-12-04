f5dd7fdb30e16a18b827ffe798f45b36b6e9ba8d
Noel Kenfack <noel.kenfack@yahoo.fr>
2023-12-04 11:30:50 +0100

    first commit

---
+++ src/test/java/com/afhunt/apiblog/ApiblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afhunt.apiblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
